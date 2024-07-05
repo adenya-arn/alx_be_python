cdb48650d57c84733366fab01393a54884bb4594
Arnold Adenya <aadenya7@gmail.com>
2024-07-05 22:56:05 +0300

    daily reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,7 +2,7 @@
 
 task = str(input("Enter your task:"))
 priority = str(input("Priority (high/medium/low):"))
-time_bound = str(input("Is it time-bound? yes/no:"))
+time = str(input("Is it time-bound? yes/no:"))
 
 match priority:
     case "high":
