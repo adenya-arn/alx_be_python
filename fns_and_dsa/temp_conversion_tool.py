f5fb23fc1d2d7ff8c661f0987ea37c83f53714c8
Arnold Adenya <aadenya7@gmail.com>
2024-07-31 18:52:45 +0300

    Fixed issue

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,4 +1,4 @@
-# Define Global Conversion Factors
+'''# Define Global Conversion Factors
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 
@@ -28,3 +28,32 @@ def main():
 if __name__ == "__main__":
     main()
 
+'''
+
+
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+
+print('Temp conversion tool')
+
+
+Temp = float(input('Enter the temperature to convert in numbers:'))
+Measure = str(input('Is this temperature in Celsius or Farenheit? (C/F)').upper())
+
+
+
+def cel_to_far(C):
+    return (C * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+
+def far_to_cel(F):
+    return (F - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+
+if Measure == 'C':
+    new_temp = f'{Temp} {Measure} is {cel_to_far}°F'
+    print (new_temp)
+elif Measure == 'F':
+    new_temp = f'{Temp} {Measure} is {far_to_cel}°C'
+    print (new_temp)
+else:
+    print('Invalid Temperature. Please enter a numeric value!!')
