2b53860b14ffcf5c07e943498bb47c3810552e79
adenya-arn <aadenya7@gmail.com>
2025-06-27 15:03:48 +0300

    Initial commit

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    current_date = datetime.now()#.strftime("%Y-%m-%d %H:%M:%S")
+
+    print(f"Current date and time: {current_date}")
+
+    days_to_add = int(input("Enter the number of days to add to the current date: "))
+    #print(type(days_to_add))
+    future_date = current_date + timedelta(days=days_to_add)
+
+    print (f"Future date: {future_date}")
+
+display_current_datetime()
---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,18 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9 
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+temperature_to_convert = float(input("Enter the temperature to convert: "))
+temperature_units = input("Is this temperature in Celsius or Fahrenheit? (C/F) ").strip().upper()
+
+def convert_to_celsius(fahrenheit):
+    temp = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR    
+    print(f"{fahrenheit}°F is {temp}°C")
+
+def convert_to_fahrenheit(celsius):
+    temp = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    print(f"{celsius}°C is {temp}°F")
+
+if temperature_units == "F":
+    convert_to_celsius(temperature_to_convert)
+elif temperature_units == "C":
+    convert_to_fahrenheit(temperature_to_convert)
